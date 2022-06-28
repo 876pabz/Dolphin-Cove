92e4a0acec5d564ca2838630adc0346140231ef1
876pabz <tracejj10@gmail.com>
2022-06-28 09:55:36 -0500

    ladning page update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var conn = require('../lib/db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Dolphin Cove' });
 });
 
 module.exports = router;
